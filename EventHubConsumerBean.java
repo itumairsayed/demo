1c9ff7f18e08a99c4c44a60faea98e325a7b1eb4
itumairsayed <40786574+itumairsayed@users.noreply.github.com>
2024-08-02 18:27:01 +0530

    Updated one

--- EventHubConsumerBean.java
+++ EventHubConsumerBean.java
@@ -1,5 +1,6 @@
 package com.example.eventhub.service;
 
+import com.azure.core.amqp.exception.AmqpException;
 import com.azure.messaging.eventhubs.EventData;
 import com.azure.messaging.eventhubs.EventHubClientBuilder;
 import com.azure.messaging.eventhubs.EventHubConsumerAsyncClient;
@@ -7,44 +8,36 @@ import com.azure.messaging.eventhubs.models.PartitionContext;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
-@WebListener
 public class EventHubConsumerBean implements ServletContextListener {
 
     private EventHubConsumerAsyncClient consumerClient;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("inside contextInitialized");
+        startMonitoring();
+    }
+
+    public void startMonitoring() {
+        System.out.println("inside startMonitoring");
         String connectionString = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
         String eventHubName = "eh1";
 
         // Initialize the consumer client
         this.consumerClient = new EventHubClientBuilder()
                 .connectionString(connectionString, eventHubName)
+                .consumerGroup("$Default")
                 .buildAsyncConsumerClient();
 
         // Start receiving events
         startReceiving();
     }
 
-//    @PostConstruct
-//    public void init() {
-//        String connectionString = "Endpoint=sb://localhost;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
-//        String eventHubName = "eh1";
-//
-//        // Initialize the consumer client
-//        this.consumerClient = new EventHubClientBuilder()
-//                .connectionString(connectionString, eventHubName)
-//                .buildAsyncConsumerClient();
-//
-//        // Start receiving events
-//        startReceiving();
-//    }
-
     private void startReceiving() {
-
         consumerClient.receive(false)
                 .subscribe(
                         partitionEvent -> {
@@ -56,19 +49,37 @@ public class EventHubConsumerBean implements ServletContextListener {
                         },
                         error -> {
                             System.err.println("Error occurred while receiving events: " + error);
-                        },
+                            handleProcessingError(error);
+                            },
                         () -> {
                             System.out.println("Finished receiving events.");
                         }
                 );
     }
 
-//    @PreDestroy
-//    public void cleanup() {
-//        if (consumerClient != null) {
-//            consumerClient.close();
-//        }
-//    }
+    private void handleProcessingError(Throwable throwable) {
+        System.out.println("inside handleProcessingError");
+        if (throwable instanceof AmqpException) {
+            AmqpException amqpException = (AmqpException) throwable;
+            System.err.println("AMQP Exception occurred: " + amqpException.getMessage());
+            // Attempt to reconnect or handle the error
+            reconnect();
+        } else {
+            System.err.println("Unexpected error occurred: " + throwable.getMessage());
+        }
+    }
+
+    private void reconnect() {
+        // Implement reconnection logic
+        stopMonitoring();
+        startMonitoring();
+    }
+
+    public void stopMonitoring() {
+        if (consumerClient != null) {
+            consumerClient.close();
+        }
+    }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
