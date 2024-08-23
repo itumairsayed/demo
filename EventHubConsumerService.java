cdb36c61e5b2eeb69ac387df4a30f07714f884ea
itumairsayed <40786574+itumairsayed@users.noreply.github.com>
2024-08-23 18:13:46 +0530

    Update EventHubConsumerService.java

--- EventHubConsumerService.java
+++ EventHubConsumerService.java
@@ -1,14 +1,21 @@
+package org.example.axis;
+
+import com.azure.core.amqp.exception.AmqpException;
 import com.azure.messaging.eventhubs.EventData;
 import com.azure.messaging.eventhubs.EventHubConsumerAsyncClient;
-import com.azure.messaging.eventhubs.models.EventPosition;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Service
 public class EventHubConsumerService {
 
     private final EventHubConsumerAsyncClient consumerClient;
+    private Disposable subscription;
 
     public EventHubConsumerService(EventHubConsumerAsyncClient consumerClient) {
         this.consumerClient = consumerClient;
@@ -16,18 +23,48 @@ public class EventHubConsumerService {
 
     @PostConstruct
     public void startConsumer() {
-        consumerClient.receive(false)
-            .subscribe(event -> processEvent(event.getData()),
-                       error -> System.err.println("Error occurred while receiving event: " + error));
+        connect();
     }
 
-    private void processEvent(EventData eventData) {
+    public void connect() {
+        subscription = consumerClient.receive(false)
+            .subscribe(
+                event -> processEvent(event.getData()),
+                error -> handleError(error));
+    }
+
+    public void processEvent(EventData eventData) {
         // Process the received event
         System.out.println("Received event: " + eventData.getBodyAsString());
     }
 
+    public void handleError(Throwable error) {
+        System.err.println("Error occurred while receiving event: " + error.getMessage());
+
+        if (error instanceof AmqpException) {
+            System.err.println("AMQP exception, attempting to reconnect...");
+            reconnect();
+        }
+    }
+
+    public void reconnect() {
+        // Dispose of the current subscription
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
+
+        // Reconnect after a short delay
+        Flux.interval(Duration.ofSeconds(10))
+            .take(1)
+            .subscribe(i -> connect());
+    }
+
     @PreDestroy
     public void shutdownConsumer() {
+        // Properly close the consumer client when the application shuts down
+        if (subscription != null) {
+            subscription.dispose();
+        }
         consumerClient.close();
     }
 }
