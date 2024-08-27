db2e7fa9d15ef9eb718f64c27031d60db9b79ed2
itumairsayed <40786574+itumairsayed@users.noreply.github.com>
2024-08-27 10:01:39 +0530

    Update EventDataUtilTest.java

--- EventDataUtilTest.java
+++ EventDataUtilTest.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.axis.message.SOAPBody;
 import org.apache.axis.message.SOAPEnvelope;
@@ -8,6 +9,9 @@ import org.mockito.MockitoAnnotations;
 import org.w3c.dom.NodeList;
 
 import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathConstants;
+
+import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -37,23 +41,38 @@ public class EventDataUtilTest {
     @Test
     public void testExtractDetails_Success() throws Exception {
         // Arrange
-        when(mockEnvelope.getBody()).thenReturn(mockBody);
-        when(mockBody.getAsString()).thenReturn("<sample><clientSessionID>123</clientSessionID><claimNumber>CLAIM-001</claimNumber></sample>");
-        
-        when(mockBody.getChildNodes().item(0).getLocalName()).thenReturn("uploadDocument");
-        
-        doReturn("123").when(eventDataUtil).extractField("//clientSessionID/text()", mockBody);
-        doReturn("CLAIM-001").when(eventDataUtil).extractField("//claimNumber/text()", mockBody);
-        // add more stubbing for other fields
-        
+        String soapContent = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
+                "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                "xmlns:soapenc=\"http://schemas.xmlsoap.org/soap/encoding/\">" +
+                "<soapenv:Body>" +
+                "<multiRef id=\"id1\">" +
+                "<clientSessionID xsi:type=\"soapenc:string\">GF002810520160331092601</clientSessionID>" +
+                "</multiRef>" +
+                "<multiRef id=\"id2\">" +
+                //"<claimNumber xsi:type=\"soapenc:string\">7977986470101423</claimNumber>" +
+                "</multiRef>" +
+                "</soapenv:Body>" +
+                "</soapenv:Envelope>";
+
+        SOAPEnvelope envelope = new SOAPEnvelope();
+        SOAPBody body = envelope.getBody();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        org.w3c.dom.Document document = builder.parse(new ByteArrayInputStream(soapContent.getBytes()));
+        body.addDocument(document);
+
+        when(mockEnvelope.getBody()).thenReturn(body);
+        when(mockBody.getAsString()).thenReturn(soapContent);
+        doReturn("GF002810520160331092601").when(eventDataUtil).extractField("//clientSessionID/text()", body);
+        doReturn(null).when(eventDataUtil).extractField("//claimNumber/text()", body);
+
         // Act
         String jsonResult = eventDataUtil.extractDetails(mockEnvelope, mockBody);
 
         // Assert
         assertNotNull(jsonResult);
-        assertTrue(jsonResult.contains("\"transactionId\":\"123\""));
-        assertTrue(jsonResult.contains("\"claimNumber\":\"CLAIM-001\""));
-        // add more assertions as needed
+        assertTrue(jsonResult.contains("\"transactionId\":\"GF002810520160331092601\""));
+        assertFalse(jsonResult.contains("\"claimNumber\":"));
     }
 
     @Test
@@ -71,12 +90,13 @@ public class EventDataUtilTest {
     @Test
     public void testExtractField_Success() throws Exception {
         // Arrange
-        when(mockBody.getAsString()).thenReturn("<sample><clientSessionID>123</clientSessionID></sample>");
-        
+        String soapContent = "<sample><clientSessionID>123</clientSessionID></sample>";
+        when(mockBody.getAsString()).thenReturn(soapContent);
+
         when(mockNodeList.getLength()).thenReturn(1);
         when(mockNodeList.item(0).getTextContent()).thenReturn("123");
         when(mockXPathExpression.evaluate(any(), eq(XPathConstants.NODESET))).thenReturn(mockNodeList);
-        
+
         // Act
         String result = eventDataUtil.extractField("//clientSessionID/text()", mockBody);
 
@@ -104,10 +124,22 @@ public class EventDataUtilTest {
     public void testSerializeEventData_Exception() {
         // Arrange
         EventData eventData = mock(EventData.class);
-        ObjectMapper mockObjectMapper = mock(ObjectMapper.class);
-        eventDataUtil = spy(new EventDataUtil());
-        doReturn(mockObjectMapper).when(eventDataUtil).getObjectMapper();
-        doThrow(JsonProcessingException.class).when(mockObjectMapper).writeValueAsString(any(EventData.class));
+        ObjectMapper mockObjectMapper = spy(new ObjectMapper());
+
+        // Mock the ObjectMapper's behavior to throw an exception when serializing
+        doThrow(new JsonProcessingException("Mock Exception") {}).when(mockObjectMapper).writeValueAsString(any(EventData.class));
+
+        eventDataUtil = new EventDataUtil() {
+            @Override
+            public String serializeEventData(EventData eventData) {
+                try {
+                    return mockObjectMapper.writeValueAsString(eventData);
+                } catch (JsonProcessingException e) {
+                    log.error("Exception occurred while serializing EventData: {}", e.getMessage());
+                }
+                return null;
+            }
+        };
 
         // Act
         String jsonString = eventDataUtil.serializeEventData(eventData);
